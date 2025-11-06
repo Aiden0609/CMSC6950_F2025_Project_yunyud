c24fde86dce13194054925a735a3592069a50280
yunyud <adndong34@gamil.com>
2025-11-06 14:36:52 -0330

    added St.Johns data since 1994

--- preprocessing.py
+++ preprocessing.py
@@ -1,13 +1,14 @@
+#%%
 import glob
 import os
 from typing import List
 
 import pandas as pd
 
-
-def aggregateCSV(path = "./data/csv", stationId = 6720):
+#%%
+def aggregateCSV(path = "./data/StJohns"):
     
-    pattern = os.path.join(path, f"station_{stationId}_daily_*.csv")
+    pattern = os.path.join(path, f"station_*_daily_*.csv")
     files: List[str] = sorted(glob.glob(pattern))
     if not files:
         raise FileNotFoundError(f"No matching files found for pattern: {pattern}")
@@ -15,17 +16,17 @@ def aggregateCSV(path = "./data/csv", stationId = 6720):
     dfs = [pd.read_csv(file) for file in files]
     all_df = pd.concat(dfs, ignore_index=True).drop_duplicates()
 
-    output_path = os.path.join(path, f"station_{stationId}_daily_combined.csv")
+    output_path = os.path.join(path, f"stjohns_daily_combined.csv")
     all_df.to_csv(output_path, index=False)
     print(f"Output path: {output_path}")
     return all_df
 
-
-def preprocessingDf(path = "./data/csv", stationId = 6720):
+#%%
+def preprocessingDf(path = "./data/StJohns"):
     
-    combined_path = os.path.join(path, f"station_{stationId}_daily_combined.csv")
+    combined_path = os.path.join(path, f"stjohns_daily_combined.csv")
     if not os.path.exists(combined_path):
-        df = aggregateCSV(path=path, stationId=stationId)
+        df = aggregateCSV(path=path)
     else:
         df = pd.read_csv(combined_path)
 
